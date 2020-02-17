b1c7e5401a09c5cfa538ce7fdbb18adccf11f940
Luis Tar Quince <luistar15@hotmail.com>
2020-02-17 15:17:00 -0500

    Add placeholders support in Lustra\Config

--- src/Lustra/Config.php
+++ src/Lustra/Config.php
@@ -2,13 +2,14 @@
 
 namespace Lustra;
 
+use Exception;
 
 class Config {
 
 	private array $config = [];
 
 
-	public function load (string $file) : void {
+	public function loadIniFile (string $file) : void {
 		$this->config = array_replace_recursive($this->config,
 			parse_ini_file($file, true, INI_SCANNER_TYPED),
 		);
@@ -21,8 +22,18 @@ class Config {
 
 	) : void {
 
-		$this->load("{$folder}/config.base.ini");
-		$this->load("{$folder}/config.{$env}.ini");
+		$this->loadIniFile("{$folder}/config.base.ini");
+		$this->loadIniFile("{$folder}/config.{$env}.ini");
+	}
+
+
+	public function exists (
+		string $section,
+		string $key
+
+	) : bool {
+
+		return isset($this->config[$section][$key]);
 	}
 
 
@@ -34,10 +45,50 @@ class Config {
 	public function get (
 		string $section,
 		string $key,
-		       $default = null
+		       $default = null,
+		 array $placeholders = []
 	) {
 
-		return $this->config[$section][$key] ?? $default;
+		$value = $this->config[$section][$key] ?? $default;
+
+		if (is_null($value)) {
+			throw new Exception(self::class . " [$section][$key] was not found");
+		}
+
+		if (count($placeholders) > 0) {
+			$value = self::replacePlaceholdersInValue($value, $placeholders);
+		}
+
+		return $value;
+	}
+
+
+	public function replacePlaceholders (array $placeholders = []) : void {
+		if (count($placeholders) > 0) {
+			foreach ($this->config as $section => $values) {
+				foreach ($values as $k => $v) {
+					if (is_string($v)) {
+						$this->config[$section][$k] = self::replacePlaceholdersInValue($v, $placeholders);
+					}
+				}
+			}
+		}
+	}
+
+
+	private static function replacePlaceholdersInValue (
+		string $value,
+		array $placeholders
+
+	) : string {
+
+		$value = str_replace(
+			array_map(fn ($k) => "{$k}", array_keys($placeholders)),
+			array_values($placeholders),
+			$value
+		);
+
+		return $value;
 	}
 
 }
