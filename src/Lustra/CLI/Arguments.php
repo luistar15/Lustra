7848557a13ea8bda7d370ca84d2e2781fb42c576
Luis Tar Quince <luistar15@hotmail.com>
2023-04-21 07:07:42 -0500

    Use declare(strict_types=1)

--- src/Lustra/CLI/Arguments.php
+++ src/Lustra/CLI/Arguments.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Lustra\CLI;
 
 use Lustra\Validator\Validator;
--- src/Lustra/Web/App.php
+++ src/Lustra/Web/App.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Lustra\Web;
 
 use Lustra\Web\Router\Router;
